5db3b444e0e1aae82b8e5222b92a2823e776dda4
saswatsam786 <saswat.sam786@gmail.com>
2023-11-06 06:04:32 +0530

    enabled firewall commands for mysql access

--- vagrant/Automated_provisioning_MacOSM1/mysql.sh
+++ vagrant/Automated_provisioning_MacOSM1/mysql.sh
@@ -33,11 +33,11 @@ sudo systemctl restart mariadb
 
 
 #starting the firewall and allowing the mariadb to access from port no. 3306
-#sudo systemctl start firewalld
-#sudo systemctl enable firewalld
-#sudo firewall-cmd --get-active-zones
-#sudo firewall-cmd --zone=public --add-port=3306/tcp --permanent
-#sudo firewall-cmd --reload
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+sudo firewall-cmd --get-active-zones
+sudo firewall-cmd --zone=public --add-port=3306/tcp --permanent
+sudo firewall-cmd --reload
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
 sudo systemctl restart mariadb
